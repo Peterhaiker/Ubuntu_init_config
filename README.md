196598a4c7c94aced9ace33279af7892e663c3d6
Peterhaiker <vim.memory@gmail.com>
2017-10-16 19:40:40 +0800

    修改了README.md文件和init.sh,使输出更美观，并加入了换源功能

--- README.md
+++ README.md
@@ -5,6 +5,16 @@
 * 安装vim  
 * 安装vpnc,git  
 * 配置vim，配置文件来自[点此跳转](https://github.com/ruchee/vimrc)  
+* 换源  
 * 安装sogou拼音  
 * 安装chromium浏览器  
 * 最后更新系统中所有软件  
+#### 配置方法  
+```
+git clone https://github.com/Peterhaiker/Ubuntu_init_config.git
+cd Ubuntu_init_config
+chmod u+x init.sh
+./init.sh
+```
+#### 错误查看  
+若发生错误，会在Ubuntu_init_config目录下创建一个myinit.log文件用于保存错误信息，查看一下就指导错在哪一步，然后只能由您手动完成那个安装了
--- init.sh
+++ init.sh
@@ -1,35 +1,108 @@
 #!/bin/bash
+0>/dev/null
+echo -e "\033[032m--------------Welcome to here---------------\033[0m"
+echo -e "\033[032mremove useless package...\033[0m"
+sudo apt-get remove libreoffice-common unity-webapps-common  thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjonggonboard deja-dup -y>/dev/null
 
-sudo apt-get remove libreoffice-common unity-webapps-common  thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjonggonboard deja-dup -y
+#if the previous command run error,then error message output to log file
+err=$?
+if (( $err != 0 ));then
+    echo "移除无用软件命令执行错误，返回码:$err">>myinit.log
+else
+    echo -e "\033[032mdone\033[0m"
+fi
 
 #install vim
 #add vim ppa to get latest vim
-sudo add-apt-repository ppa:jonathonf/vim
-sudo apt-get update 
-sudo apt-get install vim -y
+echo -e "\033[032madd vim ppa and install vim...\033[0m"
+sudo add-apt-repository ppa:jonathonf/vim>/dev/null
+err1=$?
+sudo apt-get update -y>/dev/null
+sudo apt-get install vim -y>/dev/null
+err2=$?
+if (($err1 != 0));then
+    echo "add vim ppa failed,return value:$err1"2>>myinit.log
+elif (($err2 != 0));then
+    echo "install vim failed,return value:$err2"2>>myinit.log
+else
+    echo -e "\033[032mdone\033[0m"
+fi
 
 #install git and vpnc
-sudo apt-get install vpnc git -y
-
+echo -e "\033[032minstall vpnc and git...\033[0m"
+sudo apt-get install vpnc git -y>/dev/null
+err=$?
+if (($err != 0));then
+    echo "install vpnc or git failed,return value:$err"2>>myinit.log
+else
+    echo -e "\033[032mdone\033[0m"
+fi
 #configure vim
-sudo apt-get install vim-gtk exuberant-ctags git -y
+
+echo -e "\033[032mconfigure vim...\033[0m"
+#step 1
+sudo apt-get install vim-gtk exuberant-ctags -y>/dev/null
+if (($? == 0));then
+    echo "\033[032minstall vim-gtk exuberant-ctags success\033[0m"
+else
+    echo "install vim-gtk exuberant-ctags failed"2>>myinit.log
+fi
+#step 2
 rm -rf ~/.vim ~/.vimrc
+if (($? == 0));then
+    echo "\033[032mrm ~/.vim ~/.vimrc success\033[0m"
+else
+    echo "rm ~/.vim ~/.vimrc failed"2>>myinit.log
+fi
+#step 3
 git clone https://github.com/ruchee/vimrc.git ~/vimrc
+if (($? == 0));then
+    echo "\033[032m git clone success\033[0m"
+else
+    echo "git clone failed"2>>myinit.log
+fi
+#step 4
 mv ~/vimrc/vimfiles ~/.vim
+if (($? == 0));then
+    echo "\033[032mmv ~/vimrc/vimfiles to ~/.vim success\033[0m"
+else
+    echo "mv ~/vimrc/vimfiles to ~/.vim failed"2>>myinit.log
+fi
+#step 5
 mv ~/vimrc/_vimrc ~/.vimrc
-wget https://github.com/todylu/monaco.ttf/blob/master/monaco.ttf?raw=true 
+if (($? == 0));then
+    echo "\033[032mmv ~/vimrc/_vimrc to ~/.vimrc success\033[0m"
+else
+    echo "mv ~/vimrc/_vimrc to ~/.vimrc failed"2>>myinit.log
+fi
+#step 6
+wget https://github.com/todylu/monaco.ttf/blob/master/monaco.ttf?raw=true>/dv/null
+#step 7
 mv monaco.ttf ~/.fonts
 
+#换源
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.cpy
+sudo cp ./sources.list /etc/apt/
+sudo apt-get update>/dev/null
+
 #install sogou input 
-deb http://archive.ubuntukylin.com:10006/ubuntukylintrusty main
-sudo apt-get update
-sudo apt-get install sogoupinyin
+sudo apt-get install sogoupinyin -y>/dev/null
+if (($? == 0));then
+    echo "\033[032minstall sogoupinyin success\033[0m"
+else
+    echo "install sogoupinyin failed"2>>myinit.log
+fi
 
 #install chromium brower
 sudo apt-get install chromium-browser -y
+if (($? == 0));then
+    echo "\033[032minstall chromium brower success\033[0m"
+else
+    echo "install chromium brower failed"2>>myinit.log
+fi
 
-sudo apt-get autoremove -y
-sudo apt-get upgrade -y
+sudo apt-get autoremove -y>/dev/null
+sudo apt-get upgrade -y>/dev/null
+echo "\033[032mCongratulation,all step finished,maybe some wrong happened if myinit.log file you see in the directory\033[0m"
 
 #把laod的hosts文件上传到github供以后下载，需要先学github
-#换源
